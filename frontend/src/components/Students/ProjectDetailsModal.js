75aec7ef9a8badb25811b4a5a0b17446cc5579d5
DarshitJain04 <jain.38@iitj.ac.in>
2022-12-08 00:58:43 +0530

    updated project details popup

--- frontend/src/components/Students/ProjectDetailsModal.js
+++ frontend/src/components/Students/ProjectDetailsModal.js
@@ -1,49 +1,11 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 import styles from '../../styles/components/Students/ProjectDetailsModal.module.css';
-
-// const styles = (theme) => ({
-//   root: {
-//     margin: 0,
-//     padding: theme.spacing(2),
-//   },
-//   closeButton: {
-//     position: 'absolute',
-//     right: theme.spacing(1),
-//     top: theme.spacing(1),
-//     color: theme.palette.grey[500],
-//   },
-// });
-
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          className={classes.closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
 
 const ProjectDetailsModal = ({ data }) => {
   const [open, setOpen] = React.useState(false);
@@ -66,31 +28,66 @@ const ProjectDetailsModal = ({ data }) => {
         Details
       </button>
       <Dialog
+        fullWidth
+        maxWidth="sm"
         key={data.title}
         onClose={handleClose}
         aria-labelledby={data.title}
         open={open}
       >
-        <MuiDialogTitle disableTypography className={styles.root}>
-          <Typography id={data.title} variant="h6">
-            {data.title}
-          </Typography>
-          {handleClose ? (
-            <IconButton
-              aria-label="close"
-              className={styles.closeButton}
-              onClick={handleClose}
-            >
-              <CloseIcon />
-            </IconButton>
-          ) : null}
-        </MuiDialogTitle>
-        <DialogContent dividers>
-          <Typography
-            gutterBottom
-            dangerouslySetInnerHTML={createText(data.description)}
-          ></Typography>
-        </DialogContent>
+        <div className={styles.modal}>
+          <div className={styles.header}>
+            <div className={styles.title}>{data.title}</div>
+            <div className={styles.faculty}>
+              {`${data.faculty.user.first_name} ${data.faculty.user.last_name} (${data.faculty.program_branch.name})`}
+            </div>
+            <div className={styles.closeButton}>
+              {handleClose ? (
+                <IconButton aria-label="close" onClick={handleClose}>
+                  <CloseIcon />
+                </IconButton>
+              ) : null}
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.description}>Description</div>
+            <Typography
+              gutterBottom
+              dangerouslySetInnerHTML={createText(data.description)}
+              className={styles.descriptionContent}
+            ></Typography>
+            <div className={styles.deliverables}>Deliverables</div>
+            <Typography
+              gutterBottom
+              dangerouslySetInnerHTML={createText(data.deliverables)}
+              className={styles.deliverablesContent}
+            ></Typography>
+            {data.skills && data.skills.length > 0 ? (
+              <div className={styles.skills}>Skills</div>
+            ) : null}
+            <Stack direction="row" className={styles.skillsChips}>
+              {data.skills && data.skills.length > 0
+                ? data.skills.map((skill) => {
+                    return (
+                      <Chip className={styles.skill} label={skill.skill} />
+                    );
+                  })
+                : null}
+            </Stack>
+            {data.courses && data.courses.length > 0 ? (
+              <div className={styles.courses}>Courses</div>
+            ) : null}
+            <Stack direction="row" className={styles.coursesChips}>
+              {data.courses && data.courses.length > 0
+                ? data.courses.map((course) => {
+                    return (
+                      <Chip className={styles.course} label={course.course} />
+                    );
+                  })
+                : null}
+            </Stack>
+          </div>
+        </div>
       </Dialog>
     </div>
   );
