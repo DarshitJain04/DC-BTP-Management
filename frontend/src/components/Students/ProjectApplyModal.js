75b8b5b9d135902c6b84273b77fc1d2f51e2bcbf
DarshitJain04 <jain.38@iitj.ac.in>
2022-12-10 15:39:33 +0530

    added project apply (application) form

--- frontend/src/components/Students/ProjectApplyModal.js
+++ frontend/src/components/Students/ProjectApplyModal.js
@@ -5,6 +5,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Loading from '../Loading';
 import styles from '../../styles/components/Students/ProjectApplyModal.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +14,10 @@ const ProjectApplyModal = ({ data }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const [applicationType, setApplicationType] = useState('Design Credits');
+  const [courseCodes, setCourseCodes] = useState([]);
+  const [courseCode, setCourseCode] = useState('');
+  const [resumeLink, setResumeLink] = useState('');
+  const [notes, setNotes] = useState('');
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -23,8 +28,40 @@ const ProjectApplyModal = ({ data }) => {
   };
 
   const handleApplicationType = (event) => {
-    console.log(event.target.value);
     setApplicationType(event.target.value);
+  };
+
+  const handleCourseCode = (event) => {
+    setCourseCode(event.target.value);
+  };
+
+  const handleResume = (event) => {
+    setResumeLink(event.target.value);
+  };
+
+  const handleNote = (event) => {
+    setNotes(event.target.value);
+  };
+
+  const resetForm = () => {
+    setApplicationType('Design Credits');
+    setCourseCode(courseCodes[0].course_code);
+    setResumeLink('');
+    setNotes('');
+  };
+
+  const validateResumeLink = () => {
+    if (resumeLink.length > 0) {
+      if (
+        resumeLink.match('https://drive.google.com/drive/folders/+') ||
+        resumeLink.match('https://drive.google.com/file/+')
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
   };
 
   const handleSubmit = (project_id, application_type) => {
@@ -32,12 +69,14 @@ const ProjectApplyModal = ({ data }) => {
     var form = new FormData();
     form.append('project_id', project_id);
     form.append('application_type', application_type);
+    form.append('course_code', courseCode);
+    form.append('resume_link', resumeLink);
+    form.append('notes', notes);
+    console.log(form);
     instance
       .post('/projects/student_applications/', form)
       .then((res) => {
         if (res.status === 200) {
-          console.log('Response');
-          console.log(res.data);
           handleClose();
           window.alert('Application submitted successfully');
           window.location.reload();
@@ -62,54 +101,119 @@ const ProjectApplyModal = ({ data }) => {
     }
   }, [open]);
 
+  useEffect(() => {
+    instance
+      .get('/projects/application_course_codes')
+      .then((res) => {
+        setCourseCodes(res.data);
+        setCourseCode(res.data[0].course_code);
+      })
+      .then(() => setLoading(false))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div>
-      <button className={styles.projectApply} onClick={() => handleClickOpen()}>
-        Apply
-      </button>
-      <Dialog
-        fullWidth
-        maxWidth="sm"
-        open={open}
-        onClose={() => handleClose()}
-        scroll="paper"
-        aria-labelledby={data.title}
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id={data.title}>
-          <div className={styles.title}>{data.title}</div>
-          <div className={styles.faculty}>
-            {`${data.faculty.user.first_name} ${data.faculty.user.last_name} (${data.faculty.program_branch.name})`}
-          </div>
-        </DialogTitle>
-        <DialogContent dividers="true">
-          <Form>
-            <Form.Group className="mb-3" controlId="Application Type">
-              <Form.Label className={styles.applicationType}>
-                Application Type
-              </Form.Label>
-              <Form.Select
-                aria-label="Application Type"
-                onChange={(event) => handleApplicationType(event)}
-                className={styles.dropDown}
-              >
-                <option value="Design Credits">Design Credits</option>
-                <option value="B.Tech. Project">B.Tech. Project</option>
-              </Form.Select>
-            </Form.Group>
-            <div className={styles.submit}>
-              <Button
-                variant="primary"
-                type="submit"
-                onClick={() => handleSubmit(data.id, applicationType)}
-              >
-                Submit
-              </Button>
-            </div>
-          </Form>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <button
+            className={styles.projectApply}
+            onClick={() => handleClickOpen()}
+          >
+            Apply
+          </button>
+          <Dialog
+            fullWidth
+            maxWidth="sm"
+            open={open}
+            onClose={() => handleClose()}
+            scroll="paper"
+            aria-labelledby={data.title}
+            aria-describedby="scroll-dialog-description"
+          >
+            <DialogTitle id={data.title}>
+              <div className={styles.title}>{data.title}</div>
+              <div className={styles.faculty}>
+                {`${data.faculty.user.first_name} ${data.faculty.user.last_name} (${data.faculty.program_branch.name})`}
+              </div>
+            </DialogTitle>
+            <DialogContent dividers="true">
+              <Form>
+                <Form.Group className="mb-3" controlId="Application Type">
+                  <Form.Label className={styles.applicationType}>
+                    Application Type
+                  </Form.Label>
+                  <Form.Select
+                    aria-label="Application Type"
+                    onChange={(event) => handleApplicationType(event)}
+                    className={styles.dropDown}
+                    value={applicationType}
+                  >
+                    <option value="Design Credits">Design Credits</option>
+                    <option value="B.Tech. Project">B.Tech. Project</option>
+                  </Form.Select>
+                  <Form.Label className={styles.courseCodes}>
+                    Course Code
+                  </Form.Label>
+                  <Form.Select
+                    aria-label="Course Code"
+                    onChange={(event) => handleCourseCode(event)}
+                    className={styles.courseCode}
+                    value={courseCode}
+                  >
+                    {courseCodes?.map((courseCode) => {
+                      return (
+                        <option value={courseCode.course_code}>
+                          {courseCode.course_code} ({courseCode.course_name})
+                        </option>
+                      );
+                    })}
+                  </Form.Select>
+                  <Form.Label className={styles.resume}>Resume Link</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Resume Link (Google Drive Folder/File Link)"
+                    className={styles.resumeLink}
+                    onChange={(event) => handleResume(event)}
+                    value={resumeLink}
+                  />
+                  <Form.Label className={styles.notes}>
+                    Notes (If Any)
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    value={notes}
+                    className={styles.notesTextArea}
+                    rows={6}
+                    onChange={(event) => handleNote(event)}
+                  />
+                </Form.Group>
+                <div className={styles.projectActions}>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    onClick={() => resetForm()}
+                  >
+                    Clear
+                  </Button>
+                  <Button
+                    className={styles.submitButton}
+                    variant="primary"
+                    type="submit"
+                    disabled={!validateResumeLink()}
+                    onClick={() => handleSubmit(data.id, applicationType)}
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </Form>
+            </DialogContent>
+          </Dialog>
+        </div>
+      )}
+    </>
   );
 };
 
