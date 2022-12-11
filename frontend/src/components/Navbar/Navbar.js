110cf11589a6cc86c281fd12bf94803e037486c5
unknown <santosh.2@iitj.ac.in>
2022-12-11 20:02:24 +0530

    Added Navbar

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import cclab_iitj_logo from '../../assets/cclab_iitj_logo.png';
+import { Link } from 'react-router-dom';
+import styles from '../../styles/components/Navbar/Navbar.module.css';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography
+        variant="h6"
+        sx={{ my: 2 }}
+        style={{ backgroundColor: '#8f384d', margin: '0px', padding: '15px' }}
+      >
+        <Link to="/">
+          <img
+            src={cclab_iitj_logo}
+            alt="cclab_iitj_logo"
+            width="130px"
+            heigth="64px"
+          />
+        </Link>
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem key="All Projects" disablePadding>
+          <ListItemButton sx={{ textAlign: 'center' }} href="/faculty-projects">
+            <ListItemText primary="All Projects" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Profile" disablePadding>
+          <ListItemButton
+            sx={{ textAlign: 'center' }}
+            href="/faculty-dashboard/profile"
+          >
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex', marginBottom: '55px' }}>
+      <AppBar component="nav" style={{ backgroundColor: '#8f384d' }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <Link to="/">
+              <img
+                src={cclab_iitj_logo}
+                alt="cclab_iitj_logo"
+                width="100px"
+                heigth="64px"
+              />
+            </Link>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Button
+              key="My Profile"
+              href="/faculty-dashboard/profile"
+              className={styles.navOption}
+            >
+              My Profile
+            </Button>
+            <Button
+              key="All Projects"
+              href="/faculty-projects"
+              className={styles.navOption}
+            >
+              All Projects
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
