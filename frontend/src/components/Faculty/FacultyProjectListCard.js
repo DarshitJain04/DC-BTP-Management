1ee329d93e52855c872b77aeff9c73645fa8457d
unknown <santosh.2@iitj.ac.in>
2022-12-11 20:03:10 +0530

    Added Delete Project For Faculty

--- frontend/src/components/Faculty/FacultyProjectListCard.js
+++ frontend/src/components/Faculty/FacultyProjectListCard.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import instance from '../../api/axios';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
@@ -22,6 +23,23 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const FacultyProjectListCard = ({ data }) => {
+  const handleDelete = (project_id) => {
+    instance
+      .delete(`/projects/projects_floated/${project_id}`)
+      .then((res) => {
+        if (res.status === 200) {
+          window.alert('Project deleted successfully');
+          window.location.reload();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response) {
+          window.alert(error);
+          window.location.reload();
+        }
+      });
+  };
   return (
     <FadeUpWhenVisible>
       <Paper elevation={3} className={styles.project}>
@@ -53,6 +71,7 @@ const FacultyProjectListCard = ({ data }) => {
               height: 'fit-content',
             }}
             endIcon={<DeleteIcon />}
+            onClick={() => handleDelete(data.id)}
           >
             Delete
           </Button>
